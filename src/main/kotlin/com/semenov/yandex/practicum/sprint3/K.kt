d8d4a5dd550a86ce537b12349cfbadcd36342e82
rvsemenov <rvsemenov@domclick.ru>
2024-08-04 12:15:19 +0300

    K

--- src/main/kotlin/com/semenov/yandex/practicum/sprint3/K.kt
+++ src/main/kotlin/com/semenov/yandex/practicum/sprint3/K.kt
@@ -1,30 +1,29 @@
 package com.semenov.yandex.practicum.sprint3
 
-
 fun main() {
+    test()
+}
+
+fun test() {
     val a = intArrayOf(1, 4, 9, 2, 10, 11)
     val b: IntArray = merge(a, 0, 3, 6)
     val expected = intArrayOf(1, 2, 4, 9, 10, 11)
-    println(b.contentToString())
     assert(b.contentEquals(expected))
     val c = intArrayOf(1, 4, 2, 10, 1, 2)
     merge_sort(c, 0, 6)
-    println(c.contentToString())
     val expected2 = intArrayOf(1, 1, 2, 2, 4, 10)
     assert(c.contentEquals(expected2))
 }
-
 
 fun merge(arr: IntArray, left: Int, mid: Int, right: Int): IntArray {
     val leftArr = arr.copyOfRange(left, mid)
     val rightArr = arr.copyOfRange(mid, right)
 
-
     var i = 0
     var j = 0
     var k = left
 
-    while (i < leftArr.size && j < leftArr.size) {
+    while (i < leftArr.size && j < rightArr.size) {
         if (leftArr[i] <= rightArr[j]) {
             arr[k] = leftArr[i]
             i++
