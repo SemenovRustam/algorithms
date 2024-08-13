3bb85d707b9d0c5e3dd58a504757c44201aca66a
rvsemenov <rvsemenov@domclick.ru>
2024-08-13 17:01:19 +0700

    FinalTask2

--- src/main/kotlin/com/semenov/yandex/practicum/sprint3/FinalTask2.kt
+++ src/main/kotlin/com/semenov/yandex/practicum/sprint3/FinalTask2.kt
@@ -1,6 +1,28 @@
 package com.semenov.yandex.practicum.sprint3
 
-fun quicksort(arr: IntArray, low: Int, high: Int) {
+fun main() {
+    val count = readln().toInt()
+    val members = buildList {
+        for (index in 0 until count) {
+            val info = readln().split(" ")
+            add(
+                Member(
+                    name = info[0],
+                    solvedTask = info[1].toInt(),
+                    fine = info[2].toInt()
+                )
+            )
+        }
+    }.toTypedArray()
+
+    quicksort(members, 0, members.size - 1)
+
+    members.forEach {
+        println(it)
+    }
+}
+
+fun quicksort(arr: Array<Member>, low: Int, high: Int) {
     if (low < high) {
         val pivotIndex = partition(arr, low, high)
         quicksort(arr, low, pivotIndex - 1)
@@ -8,30 +30,47 @@ fun quicksort(arr: IntArray, low: Int, high: Int) {
     }
 }
 
-fun partition(arr: IntArray, low: Int, high: Int): Int {
-    val pivot = arr[high]
+fun partition(arr: Array<Member>, low: Int, high: Int): Int {
+    val solvedTaskPivot = arr[high].solvedTask
+    val finePivot = arr[high].fine
     var i = low - 1
 
     for (j in low until high) {
-        if (arr[j] <= pivot) {
+        if (arr[j].solvedTask > solvedTaskPivot) {
             i++
             swap(arr, i, j)
+        } else if (arr[j].solvedTask == solvedTaskPivot) {
+            if (arr[j].fine <= finePivot) {
+                i++
+                swap(arr, i, j)
+            }
         }
     }
     swap(arr, i + 1, high)
     return i + 1
 }
 
-fun swap(arr: IntArray, i: Int, j: Int) {
+fun swap(arr: Array<Member>, i: Int, j: Int) {
     val temp = arr[i]
     arr[i] = arr[j]
     arr[j] = temp
 }
 
-
-fun main() {
-    val arr = intArrayOf(10, 7, 8, 9, 1, 5)
-    println("Исходный массив: ${arr.joinToString(", ")}")
-    quicksort(arr, 0, arr.size - 1)
-    println("Отсортированный массив: ${arr.joinToString(", ")}")
+data class Member(
+    val name: String,
+    val solvedTask: Int,
+    val fine: Int
+) {
+    override fun toString(): String {
+        return "$name $solvedTask $fine"
+    }
 }
+
+/**
+5
+alla 4 100
+gena 6 1000
+gosha 2 90
+rita 2 90
+timofey 4 80
+ */
