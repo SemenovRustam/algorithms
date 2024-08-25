40dc5c731dc54949fae4eb582bc8f6731cce7bae
rvsemenov <rvsemenov@domclick.ru>
2024-08-25 12:16:14 +0700

    C. Странное сравнение

--- src/main/kotlin/com/semenov/yandex/practicum/sprint4/C.kt
+++ src/main/kotlin/com/semenov/yandex/practicum/sprint4/C.kt
@@ -17,7 +17,7 @@ fun canBeReplaced(word1: String, word2: String): Boolean {
     if (word1.length != word2.length) return false
 
     val minLength = minOf(word1.length, word2.length)
-    val map = buildMap<Char, Char> {
+    buildMap<Char, Char> {
         for (index in 0 until minLength) {
             if (!this.containsKey(word1[index]) && this.containsValue(word2[index])) {
                 return false
@@ -26,27 +26,53 @@ fun canBeReplaced(word1: String, word2: String): Boolean {
         }
     }
 
-    var result = true
-    for (index in 0 until minLength) {
-        if (map[word1[index]] != word2[index]) {
-            result = false
-            break
-        }
-    }
-    return result
+    return true
 }
 
 
 /**
-agg
-xdd
+Жители Алгосского архипелага придумали новый способ сравнения строк.
+Две строки считаются равными, если символы одной из них можно заменить на символы другой так,
+что первая строка станет точной копией второй строки. При этом необходимо соблюдение двух условий:
 
+Порядок вхождения символов должен быть сохранён.
+Одинаковым символам первой строки должны соответствовать одинаковые символы второй строки. Разным символам —– разные.
+Например, если строка s = «abacaba», то ей будет равна строка t = «xhxixhx»,
+так как все вхождения «a» заменены на «x», «b» –— на «h», а «c» –— на «i».
+Если же первая строка s=«abc», а вторая t=«aaa», то строки уже не будут равны,
+так как разные буквы первой строки соответствуют одинаковым буквам второй.
+
+Формат ввода
+В первой строке записана строка s, во второй –— строка t. Длины обеих строк не превосходят 106.
+Обе строки содержат хотя бы по одному символу и состоят только из маленьких латинских букв.
+
+Строки могут быть разной длины.
+
+Формат вывода
+Выведите «YES», если строки равны (согласно вышеописанным правилам), и «NO» в ином случае.
+
+Пример 1
+Ввод
 mxyskaoghi
 qodfrgmslc
 
-aba
-xxx
+Вывод
+YES
 
-abacaba
-abacabac
+Пример 2
+
+Ввод
+agg
+xdd
+
+Вывод
+YES
+
+Пример 3
+Ввод
+agg
+xda
+
+Вывод
+NO
  */
