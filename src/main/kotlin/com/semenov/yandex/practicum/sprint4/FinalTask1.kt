21bdfe91729c9d7711a5fbd34799ac7b48fbcb09
rvsemenov <rvsemenov@domclick.ru>
2024-09-10 12:00:08 +0300

    A. Поисковая система

--- src/main/kotlin/com/semenov/yandex/practicum/sprint4/FinalTask1.kt
+++ src/main/kotlin/com/semenov/yandex/practicum/sprint4/FinalTask1.kt
@@ -1,5 +1,26 @@
 package com.semenov.yandex.practicum.sprint4
 
+/**
+https://contest.yandex.ru/contest/24414/run-report/117692917/
+
+Принцип работы: по входящим документам составляю индекс: слово -> номер документа : кол-во слов
+По словам из входящих запросов достаю из индекса номер документа и сколько раз в этом документе встречается это слово
+В отдельную мапу складываю значение по номеру дока и частоту слова
+С помощью компаратора сортирую сперва по ключу, затем по значению
+Вывожу результат на экран
+
+
+Временная сложность:
+Создание индекса: n^2 = обработка строки + проход по мапе
+Обработка запроса: n^2 = обработка строки + проход по строке + проход по мапе
+Общая = n^2
+
+Пространственная сложность:
+Создание индекса - линейная, зависит от кол-ва документов
+Обработка запросов - линейная(создание мапы documentScores), зависит от кол-ва запросов
+Общая сложность - линейная
+
+ * */
 fun main() {
     val documentCount = readln().toInt()
     val wordToDocFrequency = createWordIndex(documentCount)
@@ -12,32 +33,29 @@ fun main() {
     }
 }
 
-private fun createWordIndex(documentCount: Int): MutableMap<String, MutableMap<Int, Int>> {
-    val wordToDocFrequency = mutableMapOf<String, MutableMap<Int, Int>>()
-
+private fun createWordIndex(documentCount: Int) = buildMap<String, MutableMap<Int, Int>> {
     for (docNumber in 1..documentCount) {
-        val words = readln().split(" ")
-        val wordFrequency = words.groupingBy { it.lowercase() }.eachCount()
+        val wordFrequency = readln().split(" ")
+            .groupingBy { it.lowercase() }
+            .eachCount()
 
         for ((word, freq) in wordFrequency) {
-            val docMap = wordToDocFrequency.getOrPut(word) { mutableMapOf() }
+            val docMap = getOrPut(word) { mutableMapOf() }
             docMap[docNumber] = freq
         }
     }
-    return wordToDocFrequency
 }
 
 private fun handleRequest(
-    wordToDocFrequency: MutableMap<String, MutableMap<Int, Int>>
+    wordToDocFrequency: Map<String, MutableMap<Int, Int>>
 ): Map<Int, Int> {
-
     val documentScores = mutableMapOf<Int, Int>()
     val requestWords = readln().split(" ").map { it.lowercase() }.toSet()
 
     for (word in requestWords) {
         val docMap = wordToDocFrequency[word] ?: continue
-        for ((docNumber, freq) in docMap) {
-            documentScores[docNumber] = documentScores.getOrDefault(docNumber, 0) + freq
+        for ((docNum, freq) in docMap) {
+            documentScores[docNum] = documentScores.getOrDefault(docNum, 0) + freq
         }
     }
     return documentScores
