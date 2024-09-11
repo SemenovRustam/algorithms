888fb79dddd596bb935179a34b838871c15a1e24
rvsemenov <rvsemenov@domclick.ru>
2024-09-11 17:26:04 +0300

    B. Хеш-таблица

--- src/main/kotlin/com/semenov/yandex/practicum/sprint4/FinalTask2.kt
+++ src/main/kotlin/com/semenov/yandex/practicum/sprint4/FinalTask2.kt
@@ -1,9 +1,23 @@
 package com.semenov.yandex.practicum.sprint4
 
+
+/**
+ https://contest.yandex.ru/contest/24414/run-report/117767125/
+
+ Принцип работы:
+ Реализован класс HashTable, в котором имеется поле buckets - это Массив Списков с нодами.
+ Реализованы функции:
+ hash - которая высчитывает индекс элемента по ключу
+
+ findNode - по ключу высчитывает хеш, по которому в массиве buckets ищется нода
+
+  */
+
+
 fun main() {
     val n = readln().toInt()
-    val hashTable = HashTable()
-    val none = "NONE"
+    val hashTable = HashTable(n)
+    val none = "None"
 
     repeat(n) {
         val inputData = readln().split(" ")
@@ -12,81 +26,59 @@ fun main() {
             "put" -> {
                 hashTable.put(key.toInt(), inputData[2].toInt())
             }
-
             "get" -> {
-                hashTable.get(key.toInt())
-                    ?.also { println(it) }
-                    ?: println(none)
+                println(hashTable.get(key.toInt()) ?: none)
             }
-
             "delete" -> {
-                hashTable.delete(key.toInt())
-                    ?.also { println(it) }
-                    ?: println(none)
+                println(hashTable.delete(key.toInt()) ?: none)
             }
         }
     }
 }
 
-class HashTable(var capacity: Int = 10) {
-    private val buckets = Array<MutableList<Node>>(capacity) { mutableListOf() }
-    private var size = 0
+class HashTable(private val capacity: Int) {
+    private val buckets = Array<MutableList<Node?>>(capacity) { mutableListOf() }
 
-    private fun hash(key: Int) = key % capacity
+    private fun hash(key: Int): Int {
+        val hashValue = key % capacity
+        return if (hashValue < 0) hashValue + capacity else hashValue
+    }
 
     private fun findNode(key: Int): Node? {
         val index = hash(key)
-        val nodes = buckets[index]
-
-        for (node in nodes) {
-            if (node.key == key) {
-                return node
-            }
-        }
-        return null
+        return buckets[index].find { it?.key == key }
     }
 
-    fun get(key: Int) = findNode(key)?.value
+    fun get(key: Int): Int? {
+        return findNode(key)?.value
+    }
 
     fun put(key: Int, value: Int) {
         val index = hash(key)
         val nodes = buckets[index]
-
         val existingNode = findNode(key)
+
         if (existingNode != null) {
             existingNode.value = value
         } else {
-            val newNode = Node(key, value)
-            newNode.next = nodes.firstOrNull()
-            nodes.add(0, newNode)
-            size++
+            nodes.add(Node(key, value))
         }
     }
 
     fun delete(key: Int): Int? {
         val index = hash(key)
         val nodes = buckets[index]
-        var prev: Node? = null
-        var current: Node? = nodes.firstOrNull()
+        val iterator = nodes.iterator()
 
-        while (current != null) {
-            if (current.key == key) {
-                if (prev == null) {
-                    nodes.firstOrNull()?.next = current.next
-                } else {
-                    prev.next = current.next
-                }
-                size--
-                return current.value
+        while (iterator.hasNext()) {
+            val node = iterator.next()
+            if (node?.key == key) {
+                iterator.remove()
+                return node.value
             }
-            prev = current
-            current = current.next
         }
-
         return null
     }
 }
 
-private class Node(var key: Int, var value: Int) {
-    var next: Node? = null
-}
+private class Node(val key: Int, var value: Int)
