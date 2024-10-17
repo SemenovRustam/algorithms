c9a15d55cd22e658053dc20ce46727b03f586a06
rvsemenov <rvsemenov@domclick.ru>
2024-10-17 21:40:17 +0300

    A. Дорогая сеть

--- src/main/kotlin/com/semenov/yandex/practicum/sprint6/final_task/A.kt
+++ src/main/kotlin/com/semenov/yandex/practicum/sprint6/final_task/A.kt
@@ -1,16 +1,50 @@
 package com.semenov.yandex.practicum.sprint6.final_task
 
+/**
+ * https://contest.yandex.ru/contest/25070/run-report/121322618/
+ * Принцип работы:
+ *
+ * addedVertex(vertex: Int) - добавляет вершину в множество добавленных вершин,
+ * удаляет из множества не добавленных вершин,
+ * из графа по текущей вершине находит список инцидентных ребер,
+ * фильтрует ребра, первая вершина которых входит в множество не добавленных,
+ * добавляет такие ребра в очередь edges
+ *
+ * getMaxMst() - Из множества не добавленных в остов вершин берет первую,
+ * вызывает метод addedVertex с данной вершиной.
+ * В цикле, при условии, пока очередь не пуста и множество
+ * ребер не пустое достает максимальное ребро из очереди.
+ * Если начальная вершина этого ребра в множестве не добавленных ребер,
+ * тогда добавляет это ребро в остов и вызывает с
+ * начальное вершиной ребра функцию addedVertex.
+ *
+ * Временная сложность:
+ * addedVertex() - добавление и удаление из - 0(1),
+ * Фильтрация и добавление рёбер в edges: 0(n log m)
+ *
+ * getMaxMst():
+ * 0(E log V) - тк алгоритм использует приоритетную очередь для хранения ребер,
+ * исходящих из собранного множества остова
+ *
+ * Общая сложность - 0(E log V)
+ *
+ * Пространственная сложность:
+ * Тк алгоритм  используут память для хранения вершин и ребер, то пространственная сложность равна 0(e + m)
+ *
+ * */
+
 
 import java.util.*
 
-private val added = mutableSetOf<Int>()  // Множество вершин, уже добавленных в остов.
-private val notAdded = mutableSetOf<Int>()  // Множество вершин, ещё не добавленных в остов.
+private val added = mutableSetOf<Int>()
+private val notAdded = mutableSetOf<Int>()
 private val edges = PriorityQueue<Pair<Int, Int>> { a, b -> b.second - a.second }
-private val minimumSpanningTree = mutableListOf<Pair<Int, Int>>()  // Массив рёбер, входящих в MST.
+private val minimumSpanningTree = mutableListOf<Pair<Int, Int>>()
 private val input = readln().split(" ").map { it.toInt() }
 private val n = input[0]
 private val m = input[1]
-private val graph = List(n + 1) { mutableListOf<Pair<Int, Int>>() }  // Граф в виде списка смежности.
+private val graph = List(n + 1) { mutableListOf<Pair<Int, Int>>() }
+private const val FAILED_MESSAGE = "Oops! I did it again"
 
 fun main() {
     repeat(m) {
@@ -21,25 +55,34 @@ fun main() {
         notAdded.add(v)
     }
 
-
-    if (notAdded.isEmpty()) {
-        println("Oops! I did it again")
+    if (n > 1 && m == 0) {
+        println(FAILED_MESSAGE)
         return
     }
+    if (m == 0) {
+        println(m)
+        return
+    }
+
+    if (notAdded.isEmpty()) {
+        println(FAILED_MESSAGE)
+        return
+    }
+
     getMaxMst()
 
     val result = if (notAdded.isNotEmpty()) {
-        "Oops! I did it again"
+        FAILED_MESSAGE
     } else {
-        minimumSpanningTree.sumOf { it.second }.toString()
+        minimumSpanningTree.sumOf { it.second }
     }
 
     println(result)
 }
 
 private fun getMaxMst() {
-    val v = notAdded.first()
-    addedVertex(v)
+    val vertex = notAdded.first()
+    addedVertex(vertex)
 
     while (notAdded.isNotEmpty() && edges.isNotEmpty()) {
         val maxEdge = edges.poll()
