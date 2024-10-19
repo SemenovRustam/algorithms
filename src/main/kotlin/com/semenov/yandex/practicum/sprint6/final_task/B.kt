9d27d7562ffb0c204d485566a070a2b8ff019b7b
rvsemenov <rvsemenov@domclick.ru>
2024-10-19 12:42:09 +0300

    B. Железные дороги

--- src/main/kotlin/com/semenov/yandex/practicum/sprint6/final_task/B.kt
+++ src/main/kotlin/com/semenov/yandex/practicum/sprint6/final_task/B.kt
@@ -1,6 +1,5 @@
 package com.semenov.yandex.practicum.sprint6.final_task
 
-
 fun main() {
     val n = readln().toInt()
     val graphR = Array(n + 1) { mutableListOf<Int>() }
@@ -24,15 +23,11 @@ fun main() {
 }
 
 fun isOptimalMap(n: Int, graphR: Array<MutableList<Int>>, graphB: Array<MutableList<Int>>): Boolean {
+    val visited = Array(n + 1) { 0 } // 0 - не посещен, 1 - посещен по R, 2 - посещен по B
+
     for (i in 1 until n) {
-        val visitedR = BooleanArray(n + 1)
-        val visitedB = BooleanArray(n + 1)
-
-        dfs(graphR, i, visitedR)
-        dfs(graphB, i, visitedB)
-
-        for (j in (i + 1)..n) {
-            if (visitedR[j] && visitedB[j]) {
+        if (visited[i] == 0) {
+            if (!dfs(graphR, i, visited, 1) || !dfs(graphB, i, visited, 2)) {
                 return false
             }
         }
@@ -40,13 +35,16 @@ fun isOptimalMap(n: Int, graphR: Array<MutableList<Int>>, graphB: Array<MutableL
     return true
 }
 
-fun dfs(graph: Array<MutableList<Int>>, start: Int, visited: BooleanArray) {
-    visited[start] = true
+fun dfs(graph: Array<MutableList<Int>>, start: Int, visited: Array<Int>, type: Int): Boolean {
+    visited[start] = type
     for (neighbor in graph[start]) {
-        if (!visited[neighbor]) {
-            dfs(graph, neighbor, visited)
+        if (visited[neighbor] == 0) {
+            if (!dfs(graph, neighbor, visited, type)) {
+                return false }
+        } else if (visited[neighbor] != type) {
+            // Если сосед уже был посещен другим типом дороги
+            return false
         }
     }
+    return true
 }
-
-
