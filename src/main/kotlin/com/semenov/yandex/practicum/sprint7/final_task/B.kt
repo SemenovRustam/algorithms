26aa60f34866a41e8639a8d1b391118832715f0e
rvsemenov <rvsemenov@domclick.ru>
2024-11-05 16:44:38 +0300

    M. Рюкзак

--- src/main/kotlin/com/semenov/yandex/practicum/sprint7/final_task/B.kt
+++ src/main/kotlin/com/semenov/yandex/practicum/sprint7/final_task/B.kt
@@ -1,8 +1,37 @@
 package com.semenov.yandex.practicum.sprint7.final_task
 
+import java.util.StringTokenizer
+
+/**
+ * https://contest.yandex.ru/contest/25597/run-report/123573442/
+ *
+ * Принцип работы:
+ * isDivided:
+ * Подсчитывется сумма всех очков, если сумма нечетная,  возвращается false, тк нечетное число не получится разделить
+ * на 2.
+ * Далее находится целевая сумма - это общая сумма, разделенная на 2.
+ *
+ * Создаю булевый массив dp:
+ * 1) возможно ли собрать подмножество из элементов массива scores, сумма которого равна j.
+ * 2) базовый случай - всегда можно собрать пустое подмножество
+ * 3) от целевой суммы вычитаем score, если j - score = true, то это значит, что существует такое подмножество,
+ * сумма которого равна j - score, добавление текущего элемента score позволяет получить j
+ * 4) используется алгоритм по убыванию (от target к score)
+ * 5) ответ на исходный вопрос будет располагаться в массиве под индексом 7.
+ *
+ * Для каждого элемента score обновляем массив dp в обратном порядке (от target до score).
+ * Если dp[j - score] равно true, это означает, что существует подмножество,
+ * сумма которого равна j - score, и добавление текущего score позволяет получить сумму j.
+ *
+ *
+ * Временная сложность: O(n*t), где t - кол-во игр, t - целевая сумма(половина суммы всех элементов)
+ * Пространственная сложность: O(t), где t - целевая сумма (храним данные в булевом массиве размера t)
+ * */
+
 fun main() {
-    readln().toInt()
-    val records = readln().split(" ").map { it.toInt() }.toIntArray()
+    val n = readln().toInt()
+    val tokenizer = StringTokenizer(readln())
+    val records = IntArray(n) { tokenizer.nextToken().toInt() }
 
     val result = if (isDivided(records)) "True" else "False"
 
@@ -28,6 +57,5 @@ fun isDivided(scores: IntArray): Boolean {
             }
         }
     }
-
     return dp[target]
 }
