100ac61e8c9d163a3662835d7efb7a54664da9a7
rvsemenov <rvsemenov@domclick.ru>
2024-11-15 20:56:17 +0300

    C. Самый длинный палиндром 2

--- src/main/kotlin/com/semenov/yandex/practicum/sprint8/C.kt
+++ src/main/kotlin/com/semenov/yandex/practicum/sprint8/C.kt
@@ -2,37 +2,23 @@ package com.semenov.yandex.practicum.sprint8
 
 fun main() {
     val s = readln()
-
     val result = getMaxPalindrome(s)
     println(result)
 }
 
 fun getMaxPalindrome(s: String): String {
-    val map = s.groupingBy { it }
-        .eachCount()
-        .entries.sortedByDescending {
-            it.value
-        }
+    val dictionary = s.groupingBy { it }.eachCount()
+    var mid: Char? = null
 
-    var mid = ""
-    if (s.length % 2 != 0) {
-        mid = map.sortedBy { it.key }
-            .first { it.value == 1 }
-            .key.toString()
-    }
-
-    //строю левую сторону
     val left = buildString {
-        for ((char, value) in map) {
-            if (value % 2 == 0) {
-                repeat(value / 2) {
-                    append(char)
-                }
+        for (char in dictionary.keys.sorted()) {
+            val value = dictionary[char] ?: 0
+            append(char.toString().repeat(value / 2))
+
+            if (value % 2 != 0 && (mid == null || char < mid!!)) {
+                mid = char
             }
         }
     }
-
-    val right = left.reversed()
-
-    return left + mid + right
+    return left + (mid ?: "") + left.reversed()
 }
