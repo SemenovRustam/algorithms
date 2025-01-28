fce0fbb1b759f05b3b3a8dc4f7d8097ffcacd53d
rvsemenov <rvsemenov@domclick.ru>
2025-01-28 22:11:16 +0300

    L. МногоГоша

--- src/main/kotlin/com/semenov/yandex/practicum/sprint4/L.kt
+++ src/main/kotlin/com/semenov/yandex/practicum/sprint4/L.kt
@@ -1,50 +1,40 @@
 package com.semenov.yandex.practicum.sprint4
 
 fun main() {
-    // Чтение входных данных
     val (n, k) = readln().split(" ").map(String::toInt)
     val inputString = readln()
 
-    val base = 31L
-    val mod = 1_000_000_007L
+    val base = 345L
+    val mod = 5608713984039443L
 
-    // Основная логика
     val hashToPos = mutableMapOf<Long, Int>()
     val counter = mutableMapOf<Long, Int>()
     val result = mutableListOf<Int>()
     val power = getPower(n, base, mod)
 
-    // Вычисление начального хеша
     var hash = getHash(inputString.take(n), base, mod)
-    counter[hash] = counter.getOrDefault(hash, 0) + 1
+    counter[hash] = 1
     hashToPos[hash] = 0
 
-    // Сдвиг хеша по строке
-    for (i in 1..(inputString.length - n)) { // Изменено на `..` для включения последней подстроки
-        // Обновление хеша: убираем символ inputString[i - 1] и добавляем inputString[i + n - 1]
+    for (i in 1..(inputString.length - n)) {
         hash = (hash + mod - inputString[i - 1].code * power % mod) % mod
         hash = (hash * base % mod + inputString[i + n - 1].code) % mod
 
-        // Увеличиваем счетчик для текущего хеша
         counter[hash] = counter.getOrDefault(hash, 0) + 1
 
-        // Если это первый раз, сохраняем позицию
         if (counter[hash] == 1) {
             hashToPos[hash] = i
         }
 
-        // Если подстрока встречается k раз, добавляем позицию в результат
         if (counter[hash] == k) {
             result.add(hashToPos[hash]!!)
         }
     }
 
-    // Вывод результата
     println(result.joinToString(" "))
 }
 
-// Функция для вычисления хеша подстроки
-fun getHash(str: String, base: Long, mod: Long): Long {
+private fun getHash(str: String, base: Long, mod: Long): Long {
     var hash = 0L
     for (i in str.indices) {
         hash = (hash * base % mod + str[i].code) % mod
@@ -52,8 +42,7 @@ fun getHash(str: String, base: Long, mod: Long): Long {
     return hash
 }
 
-// Функция для вычисления степени базы
-fun getPower(n: Int, base: Long, mod: Long): Long {
+private fun getPower(n: Int, base: Long, mod: Long): Long {
     var power = 1L
     for (i in 1 until n) {
         power = (power * base) % mod
